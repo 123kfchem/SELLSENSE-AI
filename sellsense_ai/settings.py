52fa48ef71adf0363f54ee68dccb83d290e5c0dd
Erick Monyancha <ericmonyancha789@gmail.com>
2026-04-23 16:56:25 +0300

    Update settings.py

--- sellsense_ai/settings.py
+++ sellsense_ai/settings.py
@@ -53,11 +53,11 @@ WSGI_APPLICATION = "sellsense_ai.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MYSQL_DB", "sellsense"),
-        "USER": os.getenv("MYSQL_USER", "root"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", ""),
-        "HOST": os.getenv("MYSQL_HOST", "127.0.0.1"),
-        "PORT": os.getenv("MYSQL_PORT", "3306"),
+        "NAME": "123kfchem$SELLSENSE-AI",
+        "USER": "123kfchem",
+        "PASSWORD": "1234@2005Erick.,
+        "HOST": "123kfchem.mysql.pythonanywhere-services.com",
+        "PORT": "3306",
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
