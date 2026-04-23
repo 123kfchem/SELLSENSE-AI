a11d6d4b958d32e90501d6d85e679c18f6eeeb5d
Erick Monyancha <ericmonyancha789@gmail.com>
2026-04-23 23:06:27 +0300

    Update WSGI configuration to include project path

--- sellsense_ai/wsgi.py
+++ sellsense_ai/wsgi.py
@@ -1,5 +1,11 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+project_home = '/home/123kfchem/SELLSENSE-AI'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sellsense_ai.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
