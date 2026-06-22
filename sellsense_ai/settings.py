8f34be4bca1c72a23d473d0e3495a0af95a6c09e
Erick Monyancha <ericmonyancha789@gmail.com>
2026-06-23 01:46:04 +0300

    Update settings.py

--- sellsense_ai/settings.py
+++ sellsense_ai/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret-key")
 DEBUG = os.getenv("DEBUG", "False").lower() in ("1", "true", "yes")
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", ".onrender.com").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", " ").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
