28c73716229f0d4d775b8e69397925d8f1205d78
123kfchem <ericmonyancha789@gmail.com>
2026-09-08 00:50:06 +0300

    added the responsiveness of login page

--- sellsense_ai/settings.py
+++ sellsense_ai/settings.py
@@ -60,9 +60,15 @@ import os
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+    
 }
 
 
